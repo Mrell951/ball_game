0d451269c3d681fe5a4e694a759c4e90d1447eff
Mrell951 <cliptester9000@gmail.com>
2026-03-12 20:49:31 +0200

    trying to inplement blocks... (issue: y snapping not working :/)

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Game:
         # init
     def __init__(self):
         self.screen = pygame.display.set_mode((800, 600))
+        self.scroll_x = 0
         pygame.display.set_caption("ball video game")
 
         self.clock = pygame.time.Clock()
@@ -14,6 +15,8 @@ class Game:
         self.gamer = self.Player(self.screen)
         self.ground1 = self.Ground(self.screen, 1)
         self.ground2 = self.Ground(self.screen, 2)
+
+        self.block = self.Solid_object_template(16, 90, True)
 
     def mainLoop(self):
         for event in pygame.event.get():
@@ -23,15 +26,20 @@ class Game:
 
             self.gamer.playerLoop(event)
 
+        self.scroll_x += 7.3
         self.gamer.PlayerPhysics()
 
         self.gamer.PlayerColision(self.ground1.hitbox)
         self.gamer.PlayerColision(self.ground2.hitbox)
 
+        self.block.run_loop(self.scroll_x)
+
         # Rendering code
         self.screen.fill("white")
 
         self.gamer.drawPlayer()
+
+        self.block.draw(self.screen)
 
         self.ground1.drawGround()
         self.ground2.drawGround()
@@ -50,8 +58,8 @@ class Game:
         def playerLoop(self, e):
 
             if e.type == pygame.KEYDOWN: # handle for presses
-                if e.key == pygame.K_SPACE:
-                    if self.falling < 3:
+                if self.falling < 3:
+                    if e.key == pygame.K_SPACE:
                         self.gravity_side *= -1
                 
             if e.type == pygame.MOUSEBUTTONDOWN:
@@ -93,10 +101,32 @@ class Game:
             pygame.draw.rect(self.screen, (125, 125, 125), self.hitbox)
 
     class Solid_object_template: # Object template for solid objects.
-        def __init__(self, sprite, x, y, hitbox_width=64, hitbox_height=64):
-            self.sprite = sprite
+        def __init__(self, x, y, snap, hitbox_width=64, hitbox_height=64):
+            # self.sprite = sprite
             self.x_pos = x
             self.y_pos = y
+
+            self.hitbox_width = hitbox_width
+            self.hitbox_height = hitbox_height
+
+            if snap:
+                grid_x = self.x_pos // self.hitbox_width
+                grid_y = self.y_pos // self.hitbox_height
+
+
+                self.snapped_x = grid_x * self.hitbox_width
+                self.snapped_y = grid_y * self.hitbox_height
+
+
+                self.hitbox = pygame.Rect(self.snapped_x, self.snapped_y, self.hitbox_width, self.hitbox_height)
+            else:
+                self.hitbox = pygame.Rect(self.x_pos, self.y_pos, self.hitbox_width, self.hitbox_height)
+        
+        def run_loop(self, scroll_amount):
+            self.hitbox.x = (scroll_amount * -1) + self.x_pos
+        
+        def draw(self, screen):
+            pygame.draw.rect(screen, (64, 64, 100), self.hitbox)
 
     class Obsticale(Solid_object_template): # bad objects >:( (inherits from Solid_object_template)
         def __init__(self, sprite, x, y, hitbox_width=64, hitbox_height=64):
